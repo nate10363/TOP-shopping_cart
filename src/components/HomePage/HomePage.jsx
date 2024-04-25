8f6e0fbb73cccb8c7db368f51624d5a9458f63a4
Nathaniel Davenport <natedavenport103@gmail.com>
2024-04-24 21:31:30 -0400

    Fix problematic jpeg placement

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
-import img from '/Users/nathanieldavenport/Documents/TOP-React-Projects/TOP-shopping_cart/src/assets/passover-seder-plate.jpeg'
 import './HomePage.css'
+import url from '/passover-seder-plate.jpeg'
+
 
 function HomePage() {
     return (
         <div id='home-container'>
             <h1 id='home-title'>Welcome to the Matzo palace</h1>
-            <img id='home-img' src={img} alt="" />
+            <img id='home-img' src={url} alt="" />
             <h5 className='h5'>get ready for some good old fashioned price gouging</h5>
             <h5>בזמן הזה של זמן חרותנו</h5>
         </div>
